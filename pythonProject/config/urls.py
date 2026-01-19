4104f177bb2197d1e514be998ca302b576b35c25
aldanvillar <avillars23@fpcoruna.afundacion.org>
2026-01-19 09:02:15 +0100

    urls

--- pythonProject/config/urls.py
+++ pythonProject/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from biblioteca/views.py import LibroListCreateAPIView, LibroDetailAPIView
+from biblioteca.views import LibroListCreateAPIView, LibroDetailAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
